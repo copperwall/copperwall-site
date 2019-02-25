5019ec82a7f2633798bb4d05f3cf163f12bb736c
Chris Opperwall <chriso@ifixit.com>
2019-02-24 22:10:24 -0800

    Fix bug in Helmet component on blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,30 +1,30 @@
-import React from "react"
-import { graphql } from 'gatsby'
-import { Helmet, title } from "react-helmet"
-import Layout from "../components/layout"
+import React from "react";
+import { graphql } from "gatsby";
+import { Helmet } from "react-helmet";
+import Layout from "../components/layout";
 
 export default ({ data }) => {
-    const post = data.markdownRemark
-    return (
-        <Layout>
-            <Helmet>
-                <title>{title}</title>
-            </Helmet>
-            <div>
-                <h1>{post.frontmatter.title}</h1>
-                <div dangerouslySetInnerHTML={{ __html: post.html }} />
-            </div>
-        </Layout>
-    )
-}
+  const post = data.markdownRemark;
+  return (
+    <Layout>
+      <Helmet>
+        <title>{post.frontmatter.title}</title>
+      </Helmet>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  );
+};
 
 export const query = graphql`
-    query($slug: String!) {
-        markdownRemark(fields: { slug: { eq: $slug } }) {
-            html
-            frontmatter {
-                title
-            }
-        }
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
     }
+  }
 `;
