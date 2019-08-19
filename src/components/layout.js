b388b232134a50acd93d38f6f78fe1eeeb71295a
Chris Opperwall <copperwall@gmail.com>
2019-08-19 09:57:46 -0700

    Increase max-width, increase line-height

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,14 +9,16 @@ export default ({ children }) => (
     css={css`
       margin-left: auto;
       margin-right: auto;
-      max-width: ${rhythm(28)};
+      max-width: ${rhythm(30)};
       padding: ${rhythm(2)};
       padding-top: ${rhythm(1.5)};
+      line-height: ${rhythm(1.2)}
     `}
   >
     <Header />
     <div>
       {children}
+      <hr />
       <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
         <img
           alt="Creative Commons License"
