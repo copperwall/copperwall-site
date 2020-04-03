8948b80750529213ff8095c729d81b90de000f3a
Chris Opperwall <copperwall@gmail.com>
2020-04-02 19:41:39 -0700

    test: add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+    <Layout>
+        <h1>Uh we couldn't find the page you're looking for.</h1>
+    </Layout>
+)
